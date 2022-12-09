735b14a3ec9f698f2645d536fe4ac922a9ccdc05
Daniel Marques <dani31415@gmail.com>
2022-12-09 08:36:20 +0100

    Attempt to recover connection.

--- back/src/connection.ts
+++ back/src/connection.ts
@@ -1,8 +1,17 @@
 import mysql from 'promise-mysql';
 
-let gConnection: mysql.Connection;
+let gConnection: mysql.Connection | undefined;
 
 export default async function getConnection(): Promise<mysql.Connection> {
+  if (gConnection !== undefined) {
+    try {
+      await gConnection.ping();
+    } catch {
+      console.log('Connection failed');
+      gConnection = undefined;
+    }
+  }
+
   if (gConnection === undefined) {
     gConnection = await mysql.createConnection({
       host     : '192.168.0.150',
@@ -12,5 +21,5 @@ export default async function getConnection(): Promise<mysql.Connection> {
       timezone : 'Z',
     });
   }
-  return gConnection;
+  return gConnection!;
 }
