4968207ecc35ed87b58ac19417d73b1edee6d6f0
Daniel Marques <dani31415@gmail.com>
2023-02-03 17:57:39 +0100

    Added model summary.

--- front/src/components/Report.tsx
+++ front/src/components/Report.tsx
@@ -12,41 +12,103 @@ function dec(x: number) {
   return Math.round(x*1000)/1000;
 }
 
-  export default function Report() {
-    const [toggle, setToggle] = useState(true);
-    const [text, setText] = useState('');
-    const [report, setReport] : [any[], any] = useState([]);
-    const navigate = useNavigate();
+export default function Report() {
+  const [toggle, setToggle] = useState(true);
+  const [text, setText] = useState('');
+  const [modelSummary, setModelSummary] : [any[], any] = useState([]);
+  const [report, setReport] : [any[], any] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect( () => {
-        async function action() {
-          const response = await fetch(`/api/report`);
-          const json = await response.json();
-          if (json.success) {
-            setReport(json.report);
-          } else {
-            console.log('error')
-            if (json?.error === 'Unauthorized') {
-              navigate('/sso/Login?forwardTo=22&RL=1&ip2loc=US');
-              navigate(0); // refresh since /sso is outside the control of react
-            } else if (json?.error === 'Unauthenticated') {
-              setText('Authenticating...');
-              const result = await fetch("/api/ib/reauthenticate");
-              if (result.status === 200) {
-                setToggle(!toggle);
-              } else {
-                setText('Failed!');
-              }
-            } else {
-              setText(json.error);
-            }
-          }
+  function setSummary(report: any[]) {
+    let models: any[] = []
+    let data: any = null;
+
+    for (const line of report) {
+      if (data === null || data.modelName !== line.modelName) {
+        data = {
+          modelName: line.modelName,
+          mean: 0,
+          gain: 0,
+          nMean: 0,
+          nGain: 0,
+          nDays: 0,
         }
-    
-        action().catch(console.error);
-      }, [ toggle ]);
-    return (
-      <Table>
+        models.push(data);
+      }
+      if (line.marketMean > 0) {
+        data.mean += line.count * line.marketMean;
+        data.nMean += line.count;
+      }
+      if (line.gain > 0) {
+        data.gain += line.count * line.gain;
+        data.nGain += line.count;
+      }
+      data.nDays += 1;
+      data.date = line.date;
+    }
+    for (const data of models) {
+        data.gain = data.gain / data.nGain;
+        data.mean = data.mean / data.nMean;
+    }
+    // setModelSummary([models[0]]);
+    setModelSummary([models[0], models[1], models[2]]);
+  }
+
+  useEffect( () => {
+    async function action() {
+      const response = await fetch(`/api/report`);
+      const json = await response.json();
+      if (json.success) {
+        setReport(json.report);
+        setSummary(json.report);
+      } else {
+        console.log('error')
+        if (json?.error === 'Unauthorized') {
+            navigate('/sso/Login?forwardTo=22&RL=1&ip2loc=US');
+            navigate(0); // refresh since /sso is outside the control of react
+        } else if (json?.error === 'Unauthenticated') {
+            setText('Authenticating...');
+            const result = await fetch("/api/ib/reauthenticate");
+            if (result.status === 200) {
+            setToggle(!toggle);
+            } else {
+            setText('Failed!');
+            }
+        } else {
+            setText(json.error);
+        }
+      }
+    }
+
+    action().catch(console.error);
+  }, [ toggle ]);
+
+  return (<div>
+    <Table>
+      <TableContainer component={Paper}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Model</TableCell>
+            <TableCell>Gain</TableCell>
+            <TableCell>Mean</TableCell>
+            <TableCell>From</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        { modelSummary.map( item => (
+          <TableRow>
+            <TableCell style={ {fontSize:'70%'} }>{ item.modelName }</TableCell>
+            <TableCell>{ dec(item.gain) }</TableCell>
+            <TableCell>{ dec(item.mean) }</TableCell>
+            <TableCell>{ item.date }</TableCell>
+          </TableRow>
+        ))}
+        </TableBody>
+      </TableContainer>
+      <span>{ text }</span>
+    </Table>
+    <br />
+    <Table>
       <TableContainer component={Paper}>
         <TableHead>
           <TableRow>
@@ -69,5 +131,5 @@ function dec(x: number) {
       </TableContainer>
       <span>{ text }</span>
     </Table>
-  );
+  </div>);
 }
